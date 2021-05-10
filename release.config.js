60bfb9ebf8daa7d2f39497fd0d927ce0f3416d1e
André Rusakow <andre.rusakow@googlemail.com>
2021-05-10 20:17:22 +0200

    fix: try stuff

--- release.config.js
+++ release.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     branches: [
-      '+([0-9])?(.{+([0-9]),x}).x',
-      'stable',
-      {name: 'rc', prerelease: true},
-      {name: 'beta', prerelease: true},
       {name: 'alpha', prerelease: true}
     ],
     plugins: [
